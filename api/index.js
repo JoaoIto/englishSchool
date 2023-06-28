0901fbd9e9fb265395d8163eafbdd678d86db2a0
João <joaovictorpfr@gmail.com>
2023-06-28 14:09:01 -0300

    start server

--- api/index.js
+++ api/index.js
@@ -2,5 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200)
+    .send({message: 'Okay server start!'})
+})
+
+app.listen(port, () => console.log(`Start server port in: ${port}`))
